9624472335dfa150c5a0479b27fe09f9191c2ac4
Ruben Alvarez <RubenDamN1Gijon@gmail.com>
2024-11-24 20:57:44 +0100

    primera subida del proyecto adt

---
+++ Datos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EjercicioADT1_1
+{
+    internal class Datos
+    {
+        
+        
+        public Datos()
+        {
+            try { }
+            catch { }
+        
+        
+        }
+    }
+}
