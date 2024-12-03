e1c996c402e561c38f30c29c8b17ed31e9cbe845
Ruben Alvarez <RubenDamN1Gijon@gmail.com>
2024-12-03 10:02:50 +0100

    extraccion ficheros

---
+++ ExamenTema1/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ExamenTema1
+{
+    public partial class FormularioJuegos : Form
+    {
+        public FormularioJuegos()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
